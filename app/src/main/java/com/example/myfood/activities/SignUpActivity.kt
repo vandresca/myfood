897424cdce4b51f231a6e7f249b48f5000a46d82
xarim <xarim@users.noreply.github.com>
2022-11-13 19:15:32 +0100

    MyFood - Implementation updates v-0.2

--- app/src/main/java/com/example/myfood/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/myfood/activities/SignUpActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pec1.R
 
-class RegisterActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
